2be9a16ea49b3f92a1cd3141553d516c0979b52a
mrjkid <semykinaalina89@gmail.com>
2024-02-06 20:08:28 +0600

    first commit

---
+++ src/Components/Alfavit/Alfavit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alfavit = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Alfavit
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import s  from  './Header.module.css';
+
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header;
